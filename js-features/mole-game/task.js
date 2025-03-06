92cc9a621346ad1c0aa1944013f79bd37d09938a
Yuriy Zinchenko <yurazinch@gmail.com>
2025-03-06 11:38:50 +0300

    mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,10 +1,9 @@
 let board = document.querySelectorAll(".hole");
-let mole = document.getElementsByClassName("hole_has-mole");
 let dead = 0;
 let lost = 0;
-for(element of board) {
-    element.addEventListener('click', (element, mole) => {
-        if(element === mole) {
+for(let element of board) {
+    element.addEventListener('click', () => {
+        if(element.className.includes("hole hole_has-mole")) {
             dead += 1;
             document.getElementById("dead").textContent = dead;
         } else {
@@ -14,12 +13,16 @@ for(element of board) {
         if(dead >= 10) {
             window.alert('Вы победили!');
             dead = 0;
+            document.getElementById("dead").textContent = dead;
             lost = 0;
+            document.getElementById("lost").textContent = lost;
         }
         if(lost >= 5) {
             window.alert('Вы проиграли!');
             dead = 0;
+            document.getElementById("dead").textContent = dead;
             lost = 0;
+            document.getElementById("lost").textContent = lost;
         }
     }, false);
 }
