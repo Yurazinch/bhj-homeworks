aae14795ff70bc106d3b715373c4068abfbfabd1
Yuriy Zinchenko <yurazinch@gmail.com>
2025-03-30 23:05:45 +0300

    preloader OK

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,8 +1,5 @@
-const loader = document.querySelector('#loader');
+const image = document.querySelector('img');
 const items = document.querySelector('#items');
-const itemCode = document.querySelectorAll('.item__code');
-const itemValue = document.querySelectorAll('.item__value');
-const itemCurrency = document.querySelectorAll('.item__currency');
 
 let xhr = new XMLHttpRequest( );
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
@@ -11,22 +8,21 @@ xhr.send();
 xhr.onreadystatechange = () => {
   if(xhr.readyState === 4) {
     let data = JSON.parse(xhr.responseText);    
-    for(let el in data.response.Valute) {
+    for(let el in data.response.Valute) {      
+      let itemCode = data.response.Valute[el].CharCode;    
+      let itemValue = data.response.Valute[el].Value;           
       items.insertAdjacentHTML("beforeEnd",`
         <div class="item">
-        <div class="item__code">${itemCode}</div>
+        <div class="item__code">${itemCode}</div>        
         <div class="item__value">${itemValue}</div>
-        <div class="item__currency">${itemCurrency}</div>
+        <div class="item__currency">руб.</div>
         </div>
-      `);      
-      itemCode.textContent = el.CharCode;    
-      itemValue.textContent = el.Name;    
-      itemCurrency.textContent = el.Value;
+      `);
     }
   }
 }
 xhr.addEventListener('readystatechange', (e) => {
   if(xhr.readyState === xhr.DONE) {
-    loader.classList.remove('loader_active');
+    image.classList.remove('loader_active');
   }
 })
