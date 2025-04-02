b97005b8b4f6e532f2642471e3737e9ad8ba725b
Yuriy Zinchenko <yurazinch@gmail.com>
2025-04-02 11:31:33 +0300

    progressbar error 201

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -8,19 +8,19 @@ let form_Data = new FormData(form);
 fileInput.addEventListener('change', (e) => {
   fileName.textContent = fileInput.value.split('\\').pop();  
 })
-form.addEventListener('submit', upload(), false);
+form.addEventListener('submit', upload, false);
 function upload(form_Data) {
   let xhr = new XMLHttpRequest( );
   xhr.upload.onprogress = function(event) {
-    progress.value = event.loaded;
+    progress.value = event.loaded / event.total;
   }
-  xhr.onloadend = function() {
+ /* xhr.onloadend = function() {
     if (xhr.status == 200) {
       console.log("Успех");
     } else {
       console.log("Ошибка " + this.status);
     }
-  };
+  };*/
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
   xhr.send(form_Data);  
 }
