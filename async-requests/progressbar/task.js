d2ec805ba50f5385e529944204cb127db428bd59
Yuriy Zinchenko <yurazinch@gmail.com>
2025-04-07 11:52:20 +0300

    progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -3,17 +3,16 @@ let form = document.querySelector('#form');
 const fileInput = document.getElementById('file');
 let fileName = document.querySelector('.input__wrapper-desc');
 
-
-let form_Data = new FormData(form);
 fileInput.addEventListener('change', (e) => {
   fileName.textContent = fileInput.value.split('\\').pop();  
 })
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  let form_Data = new FormData(form);
   let xhr = new XMLHttpRequest( );
   xhr.upload.onprogress = function(event) {
     progress.value = event.loaded / event.total;
-  }  
+  }
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
   xhr.send(form_Data);  
-});
+})
